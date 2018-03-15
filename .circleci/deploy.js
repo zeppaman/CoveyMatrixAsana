e95cd82a5b1fa32f02c82d07f6f1f84a2f976e65
Daniele Fontani <zeppaman@users.noreply.github.com>
2018-03-15 22:03:09 +0100

    Update deploy.js

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -13,6 +13,14 @@ var config = {
    
 console.log(config);
 
+ftpDeploy.on('uploading', function(data) {
+  console.log(
+    data.totalFileCount,       // total file count being transferred 
+    data.transferredFileCount, // number of files transferred 
+    data.percentComplete,      // percent as a number 1 - 100 
+    data.filename);             // partial path with filename being uploaded 
+});
+
 ftpDeploy.deploy(config, function(err) {
     if (err) console.log(err)
     else console.log('finished');
